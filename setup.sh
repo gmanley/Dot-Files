b25017b04b1acf5e55e9da9bd10375c5fa1ea036
Gray Manley <gray.manley@gmail.com>
2012-02-15 14:30:29 -0600

    Fix to setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ CONF_ROOT=`dirname $0`
 read -p "This will overwrite your previous dotfiles, make sure you have backed them up if you wish to preserve them. Continue? (y/n) "
 cd $CONF_ROOT
 cat gitconfig-extras >> "$HOME/.gitconfig"
-find $(git ls-files) -name ".*" \( ! -name .gitignore \) -exec ln -sf {} "$HOME/" \;
+find $(git ls-files) -name ".*" \( ! -name .gitignore \) -exec ln -sf "$(pwd)/{}" "$HOME/" \;
 if [[ ${rvm_version+defined} ]]; then
   ln -sf $CONF_ROOT/*.gems $rvm_path/gemsets/
 fi
